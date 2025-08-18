49c1806c3eb970e5003761d8bc64f999059a661b
El Buen Samaritano <El Buen Samaritano@SAMARITANO-01>
2025-08-18 15:18:24 -0600

    Agregar archivos de proyecto.

---
+++ POO-Tarea5_1/Clases/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Tarea5_1.Clases
+{
+    public class Persona
+    {
+
+       private string nombre { set; get; }
+        public string apellido { set; get; }
+        public int edad { set; get; }
+        public string direccion { set; get; }
+        public string numTelefono { set; get; }
+        public string numCelular { set; get; }
+        public string email { set; get; }
+        public DateTime fechanacimiento { set; get; }
+
+        public Persona()
+        {
+            this.nombre = "JOSE";
+        }
+
+            public string getNombre()
+            {
+                return this.nombre;
+            }
+        public void setNombre(string p_nombre )
+        {
+            this.nombre = p_nombre;
+        }
+
+        }
+
+        
+        
+    
+}
---
+++ POO-Tarea5_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using POO_Tarea5_1.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_tarea5_1
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Persona persona= new Persona ();
+
+        persona.setNombre("Marcos");
+
+        Console.WriteLine(persona.getNombre());
+
+        Console.ReadLine ();
+    }
+}
