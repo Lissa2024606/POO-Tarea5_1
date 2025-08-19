1b2fa49168e119b17bb00c3118a9d66dd79d8704
Maryi corea <maryicorea3@gmail.com>
2025-08-19 15:08:52 -0600

    Add files via upload

---
+++ POO-Tarea5_1/Clases/Marcador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Tarea5_1.Clases
+{
+    public class Marcador
+    {
+        private double costoHora { set; get; }
+        private double horasTrabajadas { set; get; }
+        private double salario { set; get; }
+        public Marcador() 
+        {
+            this.costoHora = 50;
+        }
+         public double getsalario(double p_horasTrabajadas)
+        {
+            this.salario = horasTrabajadas * this.costoHora;
+            return this.salario;
+        }
+    }
+}
